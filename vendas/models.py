f9d58299bee1aba50bbe92d26f6693185fe6d003
Mayelle Carvalho <mayellecarvalhotdb@gmail.com>
2018-12-08 11:47:20 -0200

    Criação classe Vendas em Vendas

--- vendas/models.py
+++ vendas/models.py
@@ -6,3 +6,13 @@ class Produto(models.Model):
     descricao = models.CharField(max_length=100)
     qtd_estoque = models.IntegerField()
     valor_unit = models.FloatField()
+
+
+class Venda(models.Model):
+
+    data_venda = models.DateField()
+    hora_venda = models.TimeField()
+    valor_total = models.FloatField()
+    produtos = models.ManyToManyField(Produto, through= 'item_venda')
+
+
