41ba92682af398586fce245bf638f5112635955e
Mayelle Carvalho <mayellecarvalhotdb@gmail.com>
2018-12-08 11:30:04 -0200

    Criação classe Produto em Vendas

--- vendas/models.py
+++ vendas/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
 
+    descricao = models.CharField(max_length=100)
+    qtd_estoque = models.IntegerField()
+    valor_unit = models.FloatField()
