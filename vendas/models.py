271c82e8ce2c0136303d660de24217fae621ce55
MayelleCarvalho <carvalhomayelle@gmail.com>
2018-12-09 22:21:32 -0300

    Atualizações no model para venda

--- vendas/models.py
+++ vendas/models.py
@@ -24,17 +24,17 @@ class Produto(models.Model):
 
 class Venda(models.Model):
 
-    data_venda = models.DateField()
-    hora_venda = models.TimeField()
-    valor_total = models.FloatField()
+    data_venda = models.DateField(null=True, blank=True)
+    hora_venda = models.TimeField(null=True, blank=True)
+    valor_total = models.FloatField(null=True, blank=True)
     produtos = models.ManyToManyField(Produto, through= 'ItemVenda')
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='cliente')
 
 
 class ItemVenda(models.Model):
 
-    valor_total_item = models.FloatField()
-    qtd_item = models.IntegerField()
+    valor_total_item = models.FloatField(null=True, blank=True)
+    qtd_item = models.IntegerField(null=True, blank=True)
     item = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='produto')
     venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name='venda')
 
